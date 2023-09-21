26a7e2ea9be2596b29ce7f4d38250e773eb75275
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-20 22:40:32 -0300

    Subtotal & Total methods

--- src/main/java/com/mattcode/course/entities/Order.java
+++ src/main/java/com/mattcode/course/entities/Order.java
@@ -48,6 +48,8 @@ public class Order implements Serializable {
         return this;
     }
 
+
+
     public Order(){
 
     }
@@ -97,6 +99,15 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Double getTotal(){
+        double sum = 0.0;
+        for(OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+
+        return sum;
     }
 
     @Override
