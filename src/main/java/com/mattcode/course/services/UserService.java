31c684e7133beab187c7f547c46f99284015c51e
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-22 15:51:27 -0300

    Exception handling - update

--- src/main/java/com/mattcode/course/services/UserService.java
+++ src/main/java/com/mattcode/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.mattcode.course.entities.User;
 import com.mattcode.course.repositories.UserRepository;
 import com.mattcode.course.services.exceptions.DatabaseException;
 import com.mattcode.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+
+        }
     }
 
     private void updateData(User entity, User obj) {
