99f52362ea3991c68ba7809bc27cfff32445f72b
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-22 15:45:44 -0300

    Exception handling - delete

---
+++ src/main/java/com/mattcode/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.mattcode.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
